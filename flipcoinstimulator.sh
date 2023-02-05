d228d55c31a9adb8f99dfd0bedc0d6cba0749893
Yogesh <ypadalkar95@gmail.com>
2023-02-05 15:48:15 +0530

    multipleflip

--- flipcoinstimulator.sh
+++ flipcoinstimulator.sh
@@ -1,6 +1,13 @@
+heads=0
+tails=0
+for((i=0;i<=10;i++))
+do
 FLIP=$(($(($RANDOM%10))%2))
 if [ $FLIP -eq 1 ];then
-    echo "heads"
+    heads=$((heads+1))
 else
-    echo "tails"
+     tails=$((tails+1))
 fi
+done
+echo heads : $heads
+echo  tails : $tails
